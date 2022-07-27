6d64df4582948ebe065798ec29eb3197edb166a2
Roshane Miller <millerroshane44@gmail.com>
2022-07-26 20:13:18 -0400

    nav styled comp

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,41 @@ import axios from 'axios'
 import { nanoid } from 'nanoid'
 import CountryGridItem from "./CountryGridItem";
 import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+
+
+const WrapperDiv = styled.div`
+    background-color: hsl(0, 0%, 98%);
+` 
+const HeaderCont = styled.header`
+    display: flex;
+    flex-direction: column;
+    gap: 2em;
+`
+
+const InputCont = styled.div`
+    position: relative;
+`
+const Input = styled.input`
+    padding: 1em;
+    width: 90%;
+    height: 100%;
+    margin-inline: auto;
+    border-radius: 8px;
+    border: none;
+    padding-left: 5px;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`
+const Button = styled.button`
+    position: absolute;
+    content: '';
+    left: 10%;
+    top: 50%;
+    background-color: transparent;
+    transform: translate(-10%, -50%);
+    border: none;
+`
 
 const MainCont = styled.main`
     display: flex;
@@ -10,6 +45,8 @@ const MainCont = styled.main`
     align-items: center;
     gap: 5em;
 `
+
+
 
 const url = 'https://restcountries.com/v3.1/all'
 
@@ -27,17 +64,17 @@ export default function Main() {
       const painandsadness = fetchedData.map(item => <CountryGridItem data={item} key={nanoid()} />)
 
     return(
-        <>
-            <header>
-                <div className="input-cont">
-                    <input type='text' name="country" />
-                    <button></button>
-                </div>
+        <WrapperDiv>
+            <HeaderCont>
+                <InputCont>
+                    <Input></Input>
+                    <Button><FontAwesomeIcon icon={faMagnifyingGlass}/></Button>
+                </InputCont>
                 <div className="filter-countries-cont">
                     <input type='text' name="country" />
                 </div>
-            </header>
+            </HeaderCont>
             <MainCont>{painandsadness}</MainCont>
-        </>
+        </WrapperDiv>
         )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,14 +3,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMoon } from '@fortawesome/free-regular-svg-icons'
 import { faSun } from '@fortawesome/free-regular-svg-icons'
 import useToggle from "../hooks/useToggle";
+import styled from 'styled-components'
+
+const NavCont = styled.nav`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`
+
+const ThemeCont = styled.div`
+    display: flex;
+    gap: 1em;
+`
 
 export default function Nav() {
     const [darkmode, setDarkmode] = useToggle(false)
-    const themeDiv = <><FontAwesomeIcon icon={darkmode ? faSun : faMoon} /><p>Dark Mode</p></>
+    const themeDiv = <ThemeCont><FontAwesomeIcon onClick={setDarkmode} icon={darkmode ? faSun : faMoon} /><p>Dark Mode</p></ThemeCont>
     return (
-        <div onClick={setDarkmode}>
+        <NavCont>
         <h1>Where in the world?</h1>
         {themeDiv}
-        </div>
+        </NavCont>
     )
 }
